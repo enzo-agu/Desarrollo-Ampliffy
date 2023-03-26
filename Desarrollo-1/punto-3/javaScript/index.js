587f50d9b8d8aa2c5702b23d2974e4bb4e684a05
enzo agu <enzo.gustavo.agu@gmail.com>
2023-03-26 05:04:48 -0300

    update-punto-3

--- Desarrollo-1/punto-3/javaScript/index.js
+++ Desarrollo-1/punto-3/javaScript/index.js
@@ -1,30 +1,33 @@
 class Textarea {
-    constructor(containerMessage, img1, img2) {
-        this.containerMessage = containerMessage
+    constructor(textReceived, response, img1, img2) {
+        this.textReceived = textReceived
+        this.response = response
         this.img1 = img1
         this.img2 = img2
     }
     showTextAndImages(newMessage) {
-        const containerMessage = this.containerMessage
         setTimeout(() => {
-            containerMessage.classList.add('img')
-            containerMessage.innerHTML = `<h2>This is the message you sent !</h2>
-            <h3>'${newMessage}'</h3>
-            <h3> And this is the quality of our pictures. Do you like it 😃?</h3>
-            <img src=${this.img1} >
-            <img src=${this.img2}>
+            const textReceived = this.textReceived
+            const response = this.response
+            textReceived.innerHTML = `<h2>Message sent:</h2>
+            <h3>${newMessage}</h3>
             `
-            document.body.append(containerMessage)
-        },500)
+            response.innerHTML = `
+            <h3> This is the quality of our pictures. Do you like it 😃?</h3>
+            <img src=${this.img1} alt='image' title='Eiffel tower'>
+            <img src=${this.img2} alt='image' title='City'>
+            `
+        }, 1000)
     }
 }
 
-
+const textReceived = document.getElementById('textReceived')
+const response = document.getElementById('response')
 const ShowMessage = document.createElement('div')
-const TextArea = new Textarea(ShowMessage, './img/img1.jpg', './img/img2.jpg')
-
+const TextArea = new Textarea(textReceived, response, './img/img1.jpg', './img/img2.jpg')
 
 const textarea = document.getElementById('textarea')
+
 const form = document.getElementById('form')
 form.addEventListener('submit', (e) => {
     e.preventDefault()
--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 - Clase constructora que recibe una imagen por parámetro y modifica su tamaño a través del DOM, utilizando métodos de clase y eventos.
 
 ### PUNTO-2:
-- Titulos que cambian en base al tamaño del dispositivo.
-- Clase constructora que recibe un contenedor para cambiar la imagen de fondo dependiendo del tamaño del dispositivo.
+- Titulos que cambian en base al tamaño del dispositivo, a través de un evento.
+- Clase constructora que recibe un contenedor para cambiar el color de fondo dependiendo del tamaño del dispositivo.
 
 ### PUNTO-3:
 - Textarea que recibe mensajes.
