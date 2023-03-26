6bf15653e6fdf9123ab723cf6407e9973d280e4f
enzo agu <enzo.gustavo.agu@gmail.com>
2023-03-25 22:43:42 -0300

    update-punto-1

--- Desarrollo-1/punto-1/javaScript/index.js
+++ Desarrollo-1/punto-1/javaScript/index.js
@@ -5,13 +5,32 @@ class Image {
     largeSize() {
         const img = this.img
         img.classList.add('box')
-        document.body.style.backgroundColor = 'wheat'
+        document.body.style.backgroundColor = 'black'
         const newText = document.createElement('div')
         newText.classList.add('newText')
-        newText.innerHTML += `<h2>Do you like this photo?</h2>
-        <h2>Good quality?</h2>
+        newText.innerHTML += `
+        <h2>Do you like this photo?</h2>
         `
         document.body.appendChild(newText)
+        Toastify({
+            text: 'Do you want to post something? Click here!',
+            duration: 10000,
+            gravity: 'top-end',
+            position: 'left',
+            style: {
+                background: 'grey',
+            },
+            onClick: () => {
+                Toastify({
+                    text: 'Visit our site!',
+                    style: {
+                        background: 'grey',
+                    },
+                    destination: 'https://www.ampliffy.com/es/'
+                }).showToast()
+            }
+        }).showToast()
+
     }
     normalSize() {
         img.classList.remove('box')
